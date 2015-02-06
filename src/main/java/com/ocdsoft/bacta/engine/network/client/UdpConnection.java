a1d0d42fe0743979133cbde6badbf6acd7429c3b
Kyle Burkhardt <mustangcoupe69@gmail.com>
2015-02-05 20:02:18 -0700

    [Update] Remove setter

--- src/main/java/com/ocdsoft/bacta/engine/network/client/UdpConnection.java
+++ src/main/java/com/ocdsoft/bacta/engine/network/client/UdpConnection.java
@@ -1,13 +1,12 @@
 package com.ocdsoft.bacta.engine.network.client;
 
 import lombok.Getter;
-import lombok.Setter;
 
 import java.net.InetSocketAddress;
 
 public abstract class UdpConnection {
 
-	@Getter @Setter protected InetSocketAddress remoteAddress;
+	@Getter protected InetSocketAddress remoteAddress;
 
     public abstract void setState(ConnectionState state);
     public abstract ConnectionState getState();
