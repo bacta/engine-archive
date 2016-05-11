0bfe9cb8c00d143029ddc1453ff111762b723a58
Kyle Burkhardt <mustangcoupe69@gmail.com>
2016-05-10 22:02:41 -0600

    [Update] Local Commit for refactor merge

--- src/main/java/com/ocdsoft/bacta/engine/service/object/ObjectService.java
+++ src/main/java/com/ocdsoft/bacta/engine/service/object/ObjectService.java
@@ -3,9 +3,12 @@ package com.ocdsoft.bacta.engine.service.object;
 /**
  * Created by Kyle on 3/24/14.
  */
+
 public interface ObjectService<C> {
 
-    <T extends C> T createObject(long creator, String templatePath);
+    <T extends C> T createObject(String templatePath, C creator);
+
+    <T extends C> T createObject(String templatePath);
 
     <T extends C> T get(long key);
 
