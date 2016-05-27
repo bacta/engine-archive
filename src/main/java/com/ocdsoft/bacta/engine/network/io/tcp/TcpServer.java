c4ef56eeaf980671cff6eb4cc50de9462db6560a
Kyle Burkhardt <mustangcoupe69@gmail.com>
2016-05-26 23:44:15 -0600

    [Add] TCP listenener on GameServer [Add] TCP Client [Add] Message Subscription

--- src/main/java/com/ocdsoft/bacta/engine/network/io/tcp/TcpServer.java
+++ src/main/java/com/ocdsoft/bacta/engine/network/io/tcp/TcpServer.java
@@ -12,8 +12,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 
-public abstract class TcpServer implements Runnable {
-	private Logger logger = LoggerFactory.getLogger(getClass().getSimpleName());
+public abstract class TcpServer {
+	private static final Logger LOGGER = LoggerFactory.getLogger(TcpServer.class);
 	private ChannelInboundHandlerAdapter handler;
 
 	private EventLoopGroup bossGroup = new NioEventLoopGroup();
@@ -27,31 +27,35 @@ public abstract class TcpServer implements Runnable {
 		this.handler = handler;
 	}
 	
-	@Override
-    public final void run() {
-		try {
-			bootstrap.group(bossGroup, workerGroup)
-			        .channel(NioServerSocketChannel.class)
-                    .option(ChannelOption.SO_BACKLOG, 100)
-                    .option(ChannelOption.TCP_NODELAY, true)
-			        .childHandler(new ChannelInitializer<SocketChannel>() { // (4)
-                        @Override
-                        public void initChannel(SocketChannel ch) throws Exception {
-                            ch.pipeline().addFirst(new IdleStateHandler(0, 10, 0));
-                            ch.pipeline().addLast(handler);
-                        }
-                    })
-                    .childOption(ChannelOption.SO_KEEPALIVE, true);
+    public final void start() {
+
+        LOGGER.info("Starting TCP Server on port {}", port);
+
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                try {
+                    bootstrap.group(bossGroup, workerGroup)
+                            .channel(NioServerSocketChannel.class)
+                            .option(ChannelOption.SO_BACKLOG, 100)
+                            .option(ChannelOption.TCP_NODELAY, true)
+                            .childHandler(handler)
+                            .childOption(ChannelOption.SO_KEEPALIVE, true);
 
 
-			ChannelFuture f = bootstrap.bind(port).sync();
-			f.channel().closeFuture().sync();
+                    LOGGER.info("TCP Server Running on port {}", port);
 
-		} catch (InterruptedException e) {
-			logger.error(e.getMessage());
-		} finally {
-			stop();
-		}
+                    ChannelFuture f = bootstrap.bind(port).sync();
+                    f.channel().closeFuture().sync();
+
+                } catch (InterruptedException e) {
+                    LOGGER.error(e.getMessage());
+                } finally {
+                    stop();
+                }
+            }
+        }).start();
     }
 
 	public void stop() {
